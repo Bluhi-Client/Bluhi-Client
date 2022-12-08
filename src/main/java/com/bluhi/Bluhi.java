893a4f2c86827b83d79798d768376c4aa0399b9e
GamePP <callumtomkins260505@gmail.com>
2022-12-08 12:56:50 +0000

    Change to be client specific mod

--- src/main/java/com/bluhi/Bluhi.java
+++ src/main/java/com/bluhi/Bluhi.java
@@ -1,21 +1,19 @@
 package com.bluhi;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Bluhi implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+import net.minecraft.client.MinecraftClient;
+
+public class Bluhi implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Bluhi-Client");
+	public static MinecraftClient client;
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	public void onInitializeClient() {
+		LOGGER.info("Initializing Bluhi...");
 
-		LOGGER.info("Hello cunt balls world!");
+		client = MinecraftClient.getInstance();
 	}
 }
