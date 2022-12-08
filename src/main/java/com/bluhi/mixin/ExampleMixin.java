f2f802929cf10ca7850e095d0e5c56a9153cb58c
GamePP <callumtomkins260505@gmail.com>
2022-12-08 11:44:55 +0000

    update

--- src/main/java/com/bluhi/mixin/ExampleMixin.java
+++ src/main/java/com/bluhi/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.bluhi.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.bluhi.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
